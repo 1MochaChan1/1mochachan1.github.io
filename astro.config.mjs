95c30d71681e3b46ca12b7c83b6794035a09281c
Daniel Chettiar <danielc@snapwork.com>
2025-12-18 22:50:46 +0530

    hope this works

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+  site: 'https://1mochachan1.github.io',
+  base: '/lenidev-portfolio',
   vite: {
     plugins: [tailwindcss()]
   }
